cb69589339187ac39accd9a44c2147ce9f58a39a
rheinaadinda <moranisinurat@gmail.com>
2025-09-10 18:24:45 +0700

    Tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,3 +143,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSTF_TRUSTED_ORIGINS = ["https://rheina-adinda-footballnews.pbp.cs.ui.ac.id"]
